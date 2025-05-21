8881901eb4e8969126fc05a4824102d3170610e3
Bruno Santos <66684921+689241bruno@users.noreply.github.com>
2025-05-21 19:18:50 -0300

    tela entrar com estilo diferente

--- src/pages/Singin/index.js
+++ src/pages/Singin/index.js
@@ -1,87 +1,107 @@
-import React from 'react';
-import {View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
-import * as Animatable from 'react-native-animatable';
+import * as Animatable from "react-native-animatable";
+import { useNavigation } from "@react-navigation/native";
 
-export default function Signin(){
-    return(
-        <View style={styles.container}>
-            <Animatable.View animation={"fadeInLeft"} delay={500} style={styles.containerHeader}>
-                <Text style={styles.message}>Entrar</Text>
-            </Animatable.View>
+export default function Signin() {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      <Animatable.View animation={"fadeInUp"} delay={100} style={styles.header}>
+        <Text style={styles.header}>Entrar</Text>
+      </Animatable.View>
 
-            <Animatable.View animation={"fadeInUp"} style={styles.containerForm}>
-                <Text style={styles.title}>Email</Text>
-                <TextInput style={styles.input} placeholder='Digite seu email...'/>
-                <Text style={styles.title}>Senha</Text>
-                <TextInput style={styles.input} placeholder='Digite sua senha...'/>
+      <Animatable.View
+        style={styles.containerForm}
+        animation={"fadeInUp"}
+        delay={300}
+      >
+        <View style={styles.containerInput}>
+          <Text style={styles.title}>Email</Text>
+          <TextInput style={styles.input} placeholder="yourname@emai.com" />
+        </View>
 
-                <TouchableOpacity style={styles.button}>
-                    <Text style={styles.buttonText}>Acessar</Text>
-                </TouchableOpacity>
+        <View style={styles.containerInput}>
+          <Text style={styles.title}>Senha</Text>
+          <TextInput style={styles.input} placeholder="password" />
+        </View>
 
-                <TouchableOpacity style={styles.buttonRegister}>
-                    <Text style={styles.register}>Não possui uma conta? Cadastra-se</Text>
-                </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("Welcome")}>
+          <Text style={styles.link}>Não possui uma conta? Cadastra-se</Text>
+        </TouchableOpacity>
 
-            </Animatable.View>
-        </View> 
-    );
+        <TouchableOpacity style={styles.btnAcessar}>
+          <Text style={styles.btnTextAcessar}>Acessar</Text>
+        </TouchableOpacity>
+      </Animatable.View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#1179E8',
-    },
-    containerHeader: {
-        marginTop: '14%',
-        marginBottom: '8%',
-        paddingStart: '5%',
-    }, 
-    message: {
-        fontSize: 28,
-        fontWeight: 'bold',
-        color: '#FFF', 
-    }, 
-    containerForm: {
-        backgroundColor: '#FFF',
-        flex: 1,
-        borderTopLeftRadius: 25,
-        borderTopRightRadius: 25,
-        paddingStart: '5%',
-        paddingEnd: '5%',
-    }, 
-    title: {
-        fontSize: 20,
-        marginTop: 28,
-    },
-    input: {
-        borderBottomWidth: 1,
-        height: 40,
-        marginBottom: 12,
-        fontSize: 16,
-    },
-    button: {
-        backgroundColor: '#1179E8',
-        width: '100%',
-        borderRadius: 4,
-        paddingVertical: 8,
-        marginTop: 14,
-        justifyContent: 'center',
-        alignItems: 'center',
-        
-    }, 
-    buttonText: {
-        fontSize: 18, 
-        color: '#FFF',
-        fontWeight: 'bold',
-    }, 
-    buttonRegister: {
-        alignSelf: 'center',
-        marginTop: 14,
-    }, 
-    register: {
-        color: '#a1a1a1',
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#1179E8",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  header: {
+    position: "absolute",
+    top: 20,
+    alignSelf: "center",
+    fontSize: 28,
+    color: "white",
+    fontWeight: "bold",
+  },
+
+  containerForm: {
+    backgroundColor: "white",
+    width: "80%",
+    height: 500,
+    padding: 30,
+    borderRadius: 15,
+    elevation: 5,
+    justifyContent: "space-around",
+  },
+
+  containerInput: {
+    justifyContent: "space-between",
+    height: 80,
+  },
+
+  title: {
+    fontSize: 17,
+  },
+
+  input: {
+    borderBottomWidth: 1,
+  },
+
+  btnAcessar: {
+    backgroundColor: "#1179E8",
+    height: 40,
+    borderRadius: 15,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  btnTextAcessar: {
+    color: "white",
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+
+  link: {
+    fontSize: 15,
+    color: "blue",
+    textAlign: "center",
+  },
+});

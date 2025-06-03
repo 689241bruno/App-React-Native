1e6e4db95846d74b7e3ddb80c939f3b5b7011341
Bruno Santos <66684921+689241bruno@users.noreply.github.com>
2025-06-02 21:39:10 -0300

    prototipo tela de materias

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,15 @@
 import React from "react";
-import { Text, View, StyleSheet, ScrollView, Pressable, Image } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  Pressable,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+
+import * as Progress from "react-native-progress";
 
 export default function Home() {
   function MateriaBox(props) {
@@ -11,10 +21,23 @@ export default function Home() {
             <Text style={props.descricaoEstilo}>{props.descricao}</Text>
           </View>
           <View style={props.estiloSecaoDois}>
-            <Image style={props.estiloImage}>
-
-            </Image>
+            <Image style={props.estiloImage}></Image>
           </View>
+        </View>
+        <View style={props.estiloSecaoSecundaria}>
+          <Text style={{ fontSize: 15, fontWeight: "bold", color: "gray" }}>
+            {props.progressoText}
+          </Text>
+          <Progress.Bar
+            progress={props.progresso}
+            width={180}
+            height={15}
+            color="gray"
+            style={{ borderColor: "gray" }}
+          />
+          <TouchableOpacity style={props.estiloBotaoTreinar}>
+            <Text style={{ color: "white", fontWeight: "bold" }}>Treinar</Text>
+          </TouchableOpacity>
         </View>
       </View>
     );
@@ -42,67 +65,58 @@ export default function Home() {
           estiloSecaoUm={styles.secaoUm}
           estiloSecaoDois={styles.secaoDois}
           estiloImage={styles.imageMateria}
-
+          estiloSecaoSecundaria={styles.secaoSecundaria}
+          estiloBotaoTreinar={styles.botaoTreinar}
+          progresso={0.5}
+          progressoText="50%"
         />
         <MateriaBox
           estilo={styles.containerMateria}
           color="#FFF"
-          titulo="Linguagens"
+          titulo="Matemática"
           tituloEstilo={styles.tituloMateria}
-          descricao="Lingua Portuguesa e Estrangeira, Arte e  Educação física."
+          descricao="Matemática aplicada à realidade."
           descricaoEstilo={styles.descricaoMateria}
           estiloSecaoPrincipal={styles.secaoPrincipal}
           estiloSecaoUm={styles.secaoUm}
           estiloSecaoDois={styles.secaoDois}
           estiloImage={styles.imageMateria}
+          estiloSecaoSecundaria={styles.secaoSecundaria}
+          estiloBotaoTreinar={styles.botaoTreinar}
+          progresso={0.7}
+          progressoText="70%"
         />
         <MateriaBox
           estilo={styles.containerMateria}
           color="#FFF"
-          titulo="Linguagens"
+          titulo="Ciências Humanas"
           tituloEstilo={styles.tituloMateria}
-          descricao="Lingua Portuguesa e Estrangeira, Arte e  Educação física."
+          descricao="Filosofia, Geografia, História e Sociologia."
           descricaoEstilo={styles.descricaoMateria}
           estiloSecaoPrincipal={styles.secaoPrincipal}
           estiloSecaoUm={styles.secaoUm}
           estiloSecaoDois={styles.secaoDois}
           estiloImage={styles.imageMateria}
+          estiloSecaoSecundaria={styles.secaoSecundaria}
+          estiloBotaoTreinar={styles.botaoTreinar}
+          progresso={0.3}
+          progressoText="30%"
         />
         <MateriaBox
           estilo={styles.containerMateria}
           color="#FFF"
-          titulo="Linguagens"
+          titulo="Ciências da Natureza"
           tituloEstilo={styles.tituloMateria}
-          descricao="Lingua Portuguesa e Estrangeira, Arte e  Educação física."
+          descricao="Biologia Física e Química."
           descricaoEstilo={styles.descricaoMateria}
           estiloSecaoPrincipal={styles.secaoPrincipal}
           estiloSecaoUm={styles.secaoUm}
           estiloSecaoDois={styles.secaoDois}
           estiloImage={styles.imageMateria}
-        />
-        <MateriaBox
-          estilo={styles.containerMateria}
-          color="#FFF"
-          titulo="Linguagens"
-          tituloEstilo={styles.tituloMateria}
-          descricao="Lingua Portuguesa e Estrangeira, Arte e  Educação física."
-          descricaoEstilo={styles.descricaoMateria}
-          estiloSecaoPrincipal={styles.secaoPrincipal}
-          estiloSecaoUm={styles.secaoUm}
-          estiloSecaoDois={styles.secaoDois}
-          estiloImage={styles.imageMateria}
-        />
-        <MateriaBox
-          estilo={styles.containerMateria}
-          color="#FFF"
-          titulo="Linguagens"
-          tituloEstilo={styles.tituloMateria}
-          descricao="Lingua Portuguesa e Estrangeira, Arte e  Educação física."
-          descricaoEstilo={styles.descricaoMateria}
-          estiloSecaoPrincipal={styles.secaoPrincipal}
-          estiloSecaoUm={styles.secaoUm}
-          estiloSecaoDois={styles.secaoDois}
-          estiloImage={styles.imageMateria}
+          estiloSecaoSecundaria={styles.secaoSecundaria}
+          estiloBotaoTreinar={styles.botaoTreinar}
+          progresso={0.4}
+          progressoText="40%"
         />
         <View style={styles.footer}></View>
       </ScrollView>
@@ -146,14 +160,13 @@ const styles = StyleSheet.create({
   containerMateria: {
     height: 210,
     width: "100%",
-    backgroundColor: "#FFF",
     alignSelf: "center",
     borderRadius: 16,
     borderWidth: 2,
     borderColor: "gray",
     marginBottom: 30,
     display: "flex",
-    flexDirection: "row",
+    flexDirection: "column",
   },
   tituloMateria: {
     fontSize: 24,
@@ -186,7 +199,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  imageMateria: { 
+  imageMateria: {
     height: "80%",
     width: "80%",
     backgroundColor: "#696969",
@@ -195,7 +208,29 @@ const styles = StyleSheet.create({
   descricaoMateria: {
     marginTop: 10,
     color: "#696969",
-
+  },
+  secaoSecundaria: {
+    height: "35%",
+    width: "100%",
+    borderBottomLeftRadius: 14,
+    borderBottomRightRadius: 14,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingRight: 15,
+    paddingLeft: 15,
+  },
+  botaoTreinar: {
+    height: "50%",
+    width: 100,
+    backgroundColor: "gray",
+    borderRadius: 15,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    elevation: 5,
   },
   footer: {
     height: 40,
